b6a82df7cfee1765ac9a5b4056615a1d44dc42ef
Pooja Kadam <pkadam@teksystems.com>
2022-06-05 21:01:08 +0530

    All Files added

---
+++ MVVM_iOS/MVVM_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM_iOS
+//
+//  Created by Pooja Kadam on 05/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MVVM_iOS/MVVM_iOSUITests/MVVM_iOSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MVVM_iOSUITestsLaunchTests.swift
+//  MVVM_iOSUITests
+//
+//  Created by Pooja Kadam on 05/06/22.
+//
+
+import XCTest
+
+class MVVM_iOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
